c88310a3294b04aacebd358c5e06d0b04e3ee682
Felipe <felipe.boix@gmail.com>
2015-05-09 12:28:16 +0200

    clases favoritos

--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Favorito.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Favorito.java
@@ -1,8 +1,62 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
 
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+
+
+
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import edu.upc.eetac.dsa.dsaqp1415g6.api.FavoritoResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 
 public class Favorito {
+	@InjectLinks({
+		@InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "favoritos", title = "Latest favoritos", type = MediaType.FAVORITOS_API_FAVORITO_COLLECTION),
+		@InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "self edit", title = "Favorito", type = MediaType.FAVORITOS_API_FAVORITO, method = "getFavorito", bindings = @Binding(name = "idfavorito", value = "${instance.idfavorito}")) })
+private List<Link> links;
+	
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	
+	public int getIdanuncio() {
+		return idanuncio;
+	}
+	public void setIdanuncio(int idanuncio) {
+		this.idanuncio = idanuncio;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
+	int idanuncio;
+	String username;
+	private long last_modified;
+	public long getLast_modified() {
+		return last_modified;
+	}
+	public void setLast_modified(long last_modified) {
+		this.last_modified = last_modified;
+	}
+	public long getCreation_timestamp() {
+		return creation_timestamp;
+	}
+	public void setCreation_timestamp(long creation_timestamp) {
+		this.creation_timestamp = creation_timestamp;
+	}
+	private long creation_timestamp;
 
 }
--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/FavoritoCollection.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/FavoritoCollection.java
@@ -1,6 +1,71 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+
+
+
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import edu.upc.eetac.dsa.dsaqp1415g6.api.FavoritoResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 public class FavoritoCollection {
+	@InjectLinks({
+		@InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "create-favorito", title = "Create favorito", type = MediaType.FAVORITOS_API_FAVORITO),
+		@InjectLink(value = "/favoritos?before={before}", style = Style.ABSOLUTE, rel = "previous", title = "Previous favoritos", type = MediaType.FAVORITOS_API_FAVORITO_COLLECTION, bindings = { @Binding(name = "before", value = "${instance.oldestTimestamp}") }),
+		@InjectLink(value = "/favoritos?after={after}", style = Style.ABSOLUTE, rel = "current", title = "Newest favoritos", type = MediaType.FAVORITOS_API_FAVORITO_COLLECTION, bindings = { @Binding(name = "after", value = "${instance.newestTimestamp}") }) })
+	private List<Link> links;
 	
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+
+
+	private List<Favorito> favoritos;
+	private long newestTimestamp;
+	private long oldestTimestamp;
+	public long getNewestTimestamp() {
+		return newestTimestamp;
+	}
+
+	public void setNewestTimestamp(long newestTimestamp) {
+		this.newestTimestamp = newestTimestamp;
+	}
+
+	public long getOldestTimestamp() {
+		return oldestTimestamp;
+	}
+
+	public void setOldestTimestamp(long oldestTimestamp) {
+		this.oldestTimestamp = oldestTimestamp;
+	}
+	
+	public List<Favorito> getFavoritos() {
+		return favoritos;
+	}
+
+	public void setFavoritos(List<Favorito> favoritos) {
+		this.favoritos = favoritos;
+	}
+	
+	public void addFavorito(Favorito favorito){
+		favoritos.add(favorito);
+	}
+
+		
+		public FavoritoCollection() {
+			super();
+			favoritos = new ArrayList<>();
+		}
 }
