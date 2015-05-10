f818a9e6e97374db35859075116c14160cfc5673
JaviCastellvi <javiderave@gmail.com>
2015-05-10 08:50:46 +0200

    Clases Anuncio

--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Anuncio.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Anuncio.java
@@ -1,7 +1,129 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+ 
+
+
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import edu.upc.eetac.dsa.dsaqp1415g6.api.AnuncioResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 public class Anuncio {
-	
+	@InjectLinks({
+		@InjectLink(resource = AnuncioResource.class, style = Style.ABSOLUTE, rel = "anuncios", title = "Latest anuncios", type = MediaType.ANUNCIOS_API_ANUNCIO_COLLECTION),
+		@InjectLink(resource = AnuncioResource.class, style = Style.ABSOLUTE, rel = "self edit", title = "Anuncio", type = MediaType.ANUNCIOS_API_ANUNCIO, method = "getAnuncio", bindings = @Binding(name = "idanuncio", value = "${instance.idanuncio}")) })
+private List<Link> links;
+
+	public int getIdanuncio() {
+		return idanuncio;
+	}
+	public void setIdanuncio(int idanuncio) {
+		this.idanuncio = idanuncio;
+	}
+	public int getContador() {
+		return contador;
+	}
+	public void setContador(int contador) {
+		this.contador = contador;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getKm() {
+		return km;
+	}
+	public void setKm(int km) {
+		this.km = km;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public long getLast_modified() {
+		return last_modified;
+	}
+	public void setLast_modified(long last_modified) {
+		this.last_modified = last_modified;
+	}
+	public long getCreation_timestamp() {
+		return creation_timestamp;
+	}
+	public void setCreation_timestamp(long creation_timestamp) {
+		this.creation_timestamp = creation_timestamp;
+	}
+	public List<Link> getLinks() {
+		return links;
+	}
+ 
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	private int idanuncio;
+	private int contador;
+	private String username;
+	private String titulo;
+	private String descripcion;
+	private String marca;
+	private String modelo;
+	private int km;
+	private int precio;
+	private String provincia;
+	private long last_modified;
+	private long creation_timestamp;
+	private String filename;
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	private String imageURL;
 	
 
 }
--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/AnuncioCollection.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/AnuncioCollection.java
@@ -1,8 +1,90 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
+
+
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import edu.upc.eetac.dsa.dsaqp1415g6.api.AnuncioResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 
 
 public class AnuncioCollection {
+	@InjectLinks({
+		@InjectLink(resource = AnuncioResource.class, style = Style.ABSOLUTE, rel = "create-anuncio", title = "Create anuncio", type = MediaType.ANUNCIOS_API_ANUNCIO),
+		@InjectLink(value = "/anuncios?before={before}", style = Style.ABSOLUTE, rel = "before", title = "Anuncios anteriores", type = MediaType.ANUNCIOS_API_ANUNCIO_COLLECTION, bindings = { @Binding(name = "before", value = "${instance.oldestTimestamp}") }),
+		@InjectLink(value = "/anuncios?after={after}", style = Style.ABSOLUTE, rel = "after", title = "Anuncios nuevos", type = MediaType.ANUNCIOS_API_ANUNCIO_COLLECTION, bindings = { @Binding(name = "after", value = "${instance.newestTimestamp}") }) })
+	private List<Link> links;
+	private List<Anuncio> anuncios;
+	private long newestTimestamp;
+	private long oldestTimestamp;
+	private int primeranuncio;
+	public int getPrimeranuncio() {
+		return primeranuncio;
+	}
+
+	public void setPrimeranuncio(int primeranuncio) {
+		this.primeranuncio = primeranuncio;
+	}
+
+	public int getUltimoanuncio() {
+		return ultimoanuncio;
+	}
+
+	public void setUltimoanuncio(int ultimoanuncio) {
+		this.ultimoanuncio = ultimoanuncio;
+	}
+
+	private int ultimoanuncio;
+ 
 	
+	public List<Anuncio> getAnuncios() {
+		return anuncios;
+	}
+
+	public void setAnuncios(List<Anuncio> anuncios) {
+		this.anuncios = anuncios;
+	}
+	
+	public void addAnuncio(Anuncio anuncio){
+		anuncios.add(anuncio);
+	}
+
+		
+		public AnuncioCollection() {
+			super();
+			anuncios = new ArrayList<>();
+		}
+
+		public long getNewestTimestamp() {
+			return newestTimestamp;
+		}
+
+		public void setNewestTimestamp(long newestTimestamp) {
+			this.newestTimestamp = newestTimestamp;
+		}
+
+		public long getOldestTimestamp() {
+			return oldestTimestamp;
+		}
+
+		public void setOldestTimestamp(long oldestTimestamp) {
+			this.oldestTimestamp = oldestTimestamp;
+		}
+		public List<Link> getLinks() {
+			return links;
+		}
+	 
+		public void setLinks(List<Link> links) {
+			this.links = links;
+		}
 
 }
