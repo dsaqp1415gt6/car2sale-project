b0edeee8d9bd1f8443bd0b325728a0fa47d6423f
alberto-gonzalez <albertito@hotmail.com>
2015-05-10 16:54:24 +0200

    mensaje class

--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Mensaje.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/Mensaje.java
@@ -1,6 +1,77 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MensajeResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 public class Mensaje {
-
+	@InjectLinks({
+		@InjectLink(resource = MensajeResource.class, style = Style.ABSOLUTE, rel = "mensajes", title = "Latest mensajes", type = MediaType.MENSAJES_API_MENSAJE_COLLECTION),
+		@InjectLink(resource = MensajeResource.class, style = Style.ABSOLUTE, rel = "self edit", title = "Mensaje", type = MediaType.MENSAJES_API_MENSAJE, method = "getMensaje", bindings = @Binding(name = "idmensaje", value = "${instance.idmensaje}")) })
+private List<Link> links;
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	private String usuarioenvia;
+	public String getUsuarioenvia() {
+		return usuarioenvia;
+	}
+	public void setUsuarioenvia(String usuarioenvia) {
+		this.usuarioenvia = usuarioenvia;
+	}
+	
+	public String getUsuariorecibe() {
+		return usuariorecibe;
+	}
+	public void setUsuariorecibe(String usuariorecibe) {
+		this.usuariorecibe = usuariorecibe;
+	}
+	public int getAnuncio() {
+		return anuncio;
+	}
+	public void setAnuncio(int anuncio) {
+		this.anuncio = anuncio;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public long getLast_modified() {
+		return last_modified;
+	}
+	public void setLast_modified(long last_modified) {
+		this.last_modified = last_modified;
+	}
+	public long getCreation_timestamp() {
+		return creation_timestamp;
+	}
+	public void setCreation_timestamp(long creation_timestamp) {
+		this.creation_timestamp = creation_timestamp;
+	}
+	
+	public int getIdmensaje() {
+		return idmensaje;
+	}
+	public void setIdmensaje(int idmensaje) {
+		this.idmensaje = idmensaje;
+	}
+	private int idmensaje;
+	private String usuariorecibe;
+	private int anuncio;
+	private String mensaje;
+	private long last_modified;
+	private long creation_timestamp;
 
 }
--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/MensajeCollection.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/MensajeCollection.java
@@ -1,7 +1,65 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.Link;
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
 
+import edu.upc.eetac.dsa.dsaqp1415g6.api.AnuncioResource;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
 
 public class MensajeCollection {
+	@InjectLinks({
+		@InjectLink(resource = AnuncioResource.class, style = Style.ABSOLUTE, rel = "create-mensaje", title = "Create mensaje", type = MediaType.MENSAJES_API_MENSAJE),
+		@InjectLink(value = "/mensajes?before={before}", style = Style.ABSOLUTE, rel = "previous", title = "Previous mensajes", type = MediaType.MENSAJES_API_MENSAJE_COLLECTION, bindings = { @Binding(name = "before", value = "${instance.oldestTimestamp}") }),
+		@InjectLink(value = "/mensajes?after={after}", style = Style.ABSOLUTE, rel = "current", title = "Newest mensajes", type = MediaType.MENSAJES_API_MENSAJE_COLLECTION, bindings = { @Binding(name = "after", value = "${instance.newestTimestamp}") }) })
+	private List<Link> links;
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+
+
+	private List<Mensaje> mensajes;
+	private long newestTimestamp;
+	private long oldestTimestamp;
+	public long getNewestTimestamp() {
+		return newestTimestamp;
+	}
+
+	public void setNewestTimestamp(long newestTimestamp) {
+		this.newestTimestamp = newestTimestamp;
+	}
+
+	public long getOldestTimestamp() {
+		return oldestTimestamp;
+	}
+
+	public void setOldestTimestamp(long oldestTimestamp) {
+		this.oldestTimestamp = oldestTimestamp;
+	}
 	
+	public List<Mensaje> getMensajes() {
+		return mensajes;
+	}
+
+	public void setMensajes(List<Mensaje> mensajes) {
+		this.mensajes = mensajes;
+	}
+	
+	public void addMensaje(Mensaje mensaje){
+		mensajes.add(mensaje);
+	}
+
+		
+		public MensajeCollection() {
+			super();
+			mensajes = new ArrayList<>();
+		}
 }
