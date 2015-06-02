2b400448f35169914755d7e8a41bc66a12bd6ea5
alberto-gonzalez <albertito@hotmail.com>
2015-06-02 11:47:30 +0200

    user class

--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/User.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/User.java
@@ -1,6 +1,68 @@
 package edu.upc.eetac.dsa.dsaqp1415g6.api.model;
 
 public class User {
-	
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private boolean loginSuccessful;
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	private String imageURL;
+	private String filename;
+
+	public String getUsername() {
+		return username;
+	}
+ 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+ 
+	public String getPassword() {
+		return password;
+	}
+ 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+ 
+	public String getName() {
+		return name;
+	}
+ 
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public String getEmail() {
+		return email;
+	}
+ 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+ 
+	public boolean isLoginSuccessful() {
+		return loginSuccessful;
+	}
+ 
+	public void setLoginSuccessful(boolean loginSuccessful) {
+		this.loginSuccessful = loginSuccessful;
+	}
 
 }
