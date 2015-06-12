dde1af0adf45a8e8aec74c0631429e2fa38039f8
JaviCastellvi <javiderave@gmail.com>
2015-06-12 08:15:14 +0200

    Car2Sale  API Version Final

--- car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/AnunciosRootAPI.java
+++ car2sale-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g6/api/model/AnunciosRootAPI.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.Link;
 
 
 
+
 import org.glassfish.jersey.linking.InjectLink;
 import org.glassfish.jersey.linking.InjectLink.Style;
 import org.glassfish.jersey.linking.InjectLinks;
@@ -17,6 +18,7 @@ import edu.upc.eetac.dsa.dsaqp1415g6.api.MensajeResource;
 import edu.upc.eetac.dsa.dsaqp1415g6.api.AnuncioResource;
 import edu.upc.eetac.dsa.dsaqp1415g6.api.AnuncioRootAPIResource;
 import edu.upc.eetac.dsa.dsaqp1415g6.api.MediaType;
+import edu.upc.eetac.dsa.dsaqp1415g6.api.UserResource;
 
 public class AnunciosRootAPI {
 	@InjectLinks({
@@ -26,7 +28,8 @@ public class AnunciosRootAPI {
         @InjectLink(resource = MensajeResource.class, style = Style.ABSOLUTE, rel = "mensajes", title = "Create new Mensaje", type = MediaType.MENSAJES_API_MENSAJE_COLLECTION),
         @InjectLink(resource = MensajeResource.class, style = Style.ABSOLUTE, rel = "create-mensaje", title = "Create new Mensaje", type = MediaType.MENSAJES_API_MENSAJE),
         @InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "favoritos", title = "Create new Favorito", type = MediaType.FAVORITOS_API_FAVORITO_COLLECTION),
-        @InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "create-favorito", title = "Create new Favorito", type = MediaType.FAVORITOS_API_FAVORITO)})
+        @InjectLink(resource = FavoritoResource.class, style = Style.ABSOLUTE, rel = "create-favorito", title = "Create new Favorito", type = MediaType.FAVORITOS_API_FAVORITO),
+        @InjectLink(resource = UserResource.class, style = Style.ABSOLUTE, rel = "login", title = "login", type = MediaType.ANUNCIOS_API_USER, method = "login"),})
 	private List<Link> links;
 
 public List<Link> getLinks() {
